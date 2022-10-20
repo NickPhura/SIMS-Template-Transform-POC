88d0c0f0a21f651222e7f5ba545335a7b34f15f1
Nick Phura <nickphura@gmail.com>
2022-10-20 14:40:49 -0700

    Add schema validation check to index.ts

--- api/src/transform-schema.test.ts
+++ api/src/transform-schema.test.ts
@@ -1,7 +1,6 @@
 import Ajv from 'ajv';
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { schema } from './schema/moose_srb_schema';
 import { transformationJSONSchema } from './transform-schema';
 
 describe('transformationJSONSchema', () => {
@@ -9,9 +8,5 @@ describe('transformationJSONSchema', () => {
 
   it('is valid json schema', () => {
     expect(ajv.validateSchema(transformationJSONSchema)).to.be.true;
-  });
-
-  it('is valid moose schema', () => {
-    expect(ajv.validate(transformationJSONSchema, schema), JSON.stringify(ajv.errors)).to.be.true;
   });
 });
