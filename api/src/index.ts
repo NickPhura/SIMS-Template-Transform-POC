bca5515c4ad323ce36ad04e16e59324b06cce84a
AlfredRosenthal <alfred.rosenthal@quartech.com>
2022-10-24 16:27:17 -0700

    added some comments

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,20 @@ import { schema } from './schema/moose_non_srb_schema';
 import { transformationJSONSchema } from './transform-schema';
 import { XLSXTransform } from './transform-utils';
 
+
+/*
+  Transect
+    - marked animals need to be added somehow
+
+  NON SRB
+    - Snow cover measurements need to be added
+    - marked animals need to be added
+  
+  SRB
+    - incidentals need to be added
+    - marked animals need to be added
+    - measurement and facts need to be added
+*/
 const TEMPLATE_NAME = 'moose_non_srb.xlsx';
 const TEMPLATE_SCHEMA = schema;
 
